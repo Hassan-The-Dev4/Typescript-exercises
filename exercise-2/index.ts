ce01c29d5613aa39638a76c13753abf2b6aae1b5
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-08-31 17:48:40 +0300

    submitted exercise 2

---
+++ exercise-2/index.ts
@@ -0,0 +1,39 @@
+
+
+// Question 1
+
+// with typescript error
+// let names: string[] = ["Ali", "Abid"];
+// names.push(90)
+
+// let statuses: boolean[] = [true, false];
+// statuses.push(60)
+
+// let grades: number[] = [80, 90, 60];
+// grades.push("Ali")
+
+
+//without typescript error
+// let names: string[] = ["Ali", "Abid"];
+// names.push("Mohamed")
+
+// let statuses: boolean[] = [true, false];
+// statuses.push(false)
+
+// let grades: number[] = [80, 90, 60];
+// grades.push(100)
+
+//Question 2
+
+//with error
+// let products: string[] = ["phone", "Laptop", 99];
+
+// without error
+// let products: string[] = ["phone", "Laptop"]
+
+
+
+//Question 3
+
+let Locations:[string, number, number] = ["London", 90, 180];
+console.log(Locations);
