9a2b54b5e9389bc0309c324a79473ef33a76ebcb
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-09-09 09:25:03 +0300

    submitted exercise 6

---
+++ exercise-6/index.ts
@@ -0,0 +1,34 @@
+// // 1. UserRole Enum Usage
+
+// // Create an enum called UserRole
+// enum UserRole {
+//     SuperAdmin = "superadmin",
+//     Moderator = "moderator",
+//     Viewer = "viewer"
+// }
+
+// function canEdit(role: UserRole): boolean {
+//     return role !== UserRole.Viewer;
+// }
+
+// console.log("Superadmin can edit:", canEdit(UserRole.SuperAdmin));
+// console.log("Moderator can edit:", canEdit(UserRole.Moderator));
+// console.log("Viewer can edit:", canEdit(UserRole.Viewer));
+
+
+
+
+
+
+
+
+
+// 2.Type Assertion with "as"
+
+const button = document.querySelector("button") as HTMLButtonElement | null; // Handle a missing button safely
+if (button) {
+  button.disabled = true;
+  console.log("Button disabled:", button.disabled);
+} else {
+  console.log("Button was not found.");
+}
